82a0af6db72ddef6301b851c390ed16a08812dbe
viettp29 <vietkaka9x@gmail.com>
2021-01-14 22:31:51 +0700

    xóa hết

--- nhap2.js
+++
@@ -1,8 +0,0 @@
-const tepghichu = 'tepghichu';
-var fs = require('fs');
-const { isString } = require('util');
-var prompt = require('prompt-sync')({
-    history: require('prompt-sync-history')()
-});
-
-
